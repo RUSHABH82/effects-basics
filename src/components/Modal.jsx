e63ded04bbd3774615f0ae6fc391de6dad95d56e
RUSHABH82 <makanirushabh0786@gmail.com>
2024-02-24 17:18:09 +0530

    use state for model closing.

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,26 +1,20 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
-import { createPortal } from 'react-dom';
+import {useEffect, useRef} from 'react';
+import {createPortal} from 'react-dom';
 
-const Modal = forwardRef(function Modal({ children }, ref) {
-  const dialog = useRef();
+const Modal = function Modal({open, onClose, children}) {
+    const dialog = useRef();
 
-  useImperativeHandle(ref, () => {
-    return {
-      open: () => {
-        dialog.current.showModal();
-      },
-      close: () => {
-        dialog.current.close();
-      },
-    };
-  });
+    useEffect(() => {
+        if (open) {
+            dialog.current.showModal();
+        } else {
+            dialog.current.close();
+        }
+    }, [open]);
 
-  return createPortal(
-    <dialog className="modal" ref={dialog}>
-      {children}
-    </dialog>,
-    document.getElementById('modal')
-  );
-});
+    return createPortal(<dialog className="modal" onClose={onClose} ref={dialog}>
+        {children}
+    </dialog>, document.getElementById('modal'));
+}
 
 export default Modal;
